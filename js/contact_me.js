442512afd3f7d496d6ebfdd37262116394fbb6b6
Ilona Parkhomenko <dn121274piv@gmail.com>
2018-07-03 01:02:02 +0300

    contact_me.min.js and contact_me.js are changed

--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Ваша заявка отправлена. Ожидайте звонка </strong>");
+                        .append("<strong> Ваша заявка была отправлена. Ожидайте звонка! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
                     //clear all fields
@@ -46,7 +46,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Извините " + firstName + ", что-то случилось с сервисом. Пожалуйста, попробуйте позже!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Извините " + firstName + ", сервис временно не работает. Пожалуйста, попробуйте позже!"));
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
